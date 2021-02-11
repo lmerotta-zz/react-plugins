ce620e27f59c46e588183543a0e2311c1b849702
Lucio Merotta <l.merotta@gmail.com>
2021-02-11 10:52:12 +0100

    Updates

---
+++ precache-manifest.297341d57b80afa1cb6bf43f3ae464d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a68ef37869f9b980dbdf1001d7c73b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7de41e3f83fa9133386d",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "c1de645665296fa54830",
+    "url": "./static/js/2.8d8dd2d2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8d8dd2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de41e3f83fa9133386d",
+    "url": "./static/js/main.3eee63cc.chunk.js"
+  },
+  {
+    "revision": "eaa2c52dc35cc92f22c9",
+    "url": "./static/js/runtime-main.3e71eba9.js"
+  }
+]);
